afee4f36e5717c586dde1749cb3b05b4661681ff
chals24x <84355471+ChalsLapWork@users.noreply.github.com>
2023-12-05 22:31:14 -0600

    v"0.1.7"  10;30pm
    oid loop() {
      switch(estado){
        case 1:if(Switch_de_Alto_Voltaje(ON,3))estado++;break; //3 segundos
        case 2:if(Bobina_de_Gas(ON))estado++;break;//encender Bobina de Gas
        case 3:if(Monitor_Temperatura())estado++;break;//cuenta el tiempo que tarda en llegar la interrupcion del rele de temp.
        case 4:Ventilador(ON);estado++;break;
        case 5:if(Monitor_Temperatura2())estado++;break;
        case 6:break;//estado de Error
        default:estado=1;break;}//fin de switch loop
    }//fin loop---------------------------------------------------------

--- HotWater_Control/HotWater_Control.ino
+++ HotWater_Control/HotWater_Control.ino
@@ -11,12 +11,15 @@
 |   1    |    1   |    0   |  External clock source no T1 pin.  |
 |   1    |    1   |    1   |  External clock source no T1 pin.  |
 */
-#define version "0.1.5"
+#define version "0.1.8"
 #define TIMER1_LED  13
 #define BOBINA_SOLENOIDE_GAS_LED 12//Solenoide que abre la llave de gas
 #define SWITCH_ENERGIA_PRINC_LED 11//switch que activa la chispa alto voltaje para que encienda el gas
 #define SWITCH_RESIST_HOTWAT_LED 10//Resistencia que calienta la agua en la regadera
 #define POWER_CONTROL_LED         9//Controla la base del MOSFET que alimenta los transistores que activan los reles
+
+#define ON  1
+#define OFF 0
 
 volatile boolean timer1_out = HIGH;
 volatile boolean timer2_out = HIGH;
@@ -24,25 +27,22 @@ volatile boolean timer3_out = HIGH;
 volatile boolean timer4_out = HIGH;
 
 
-unsigned char count1,count2,count3;
+unsigned char count1,count2,count3,estado;
+struct _Control_Salida{
+  int time1;//en cuando dura el estado deseado
+  int state;//estado deseado.
+  }swHV;
 
 
 // Timer1 interrupt-----------------------------------------------
 ISR (TIMER1_COMPA_vect) {
   digitalWrite(TIMER1_LED, timer1_out);
   timer1_out = !timer1_out;
- 
-  if(++count1>2){count1=0;
-      digitalWrite(BOBINA_SOLENOIDE_GAS_LED,timer2_out);
-      timer2_out=!timer2_out;}
 
-  if(++count2>3){count2=0;
-      digitalWrite(SWITCH_ENERGIA_PRINC_LED,timer3_out);
-      timer3_out=!timer3_out;}
+  if(swHV.time1>0){
+      if(--swHV.time1==0){
+             digitalWrite(SWITCH_ENERGIA_PRINC_LED,!swHV.state);}}
 
-  if(++count3>4){count3=0;
-      digitalWrite(SWITCH_RESIST_HOTWAT_LED,timer4_out);
-      timer4_out=!timer4_out;}
   
 }//fin de insterrupcion del timer1-----------------------------
 
@@ -56,11 +56,55 @@ void setup() {
   delay(3000);
   setupTimer();
   setTimer1(3);
-  digitalWrite(POWER_CONTROL_LED,1);//se activan los reles.
+  digitalWrite(BOBINA_SOLENOIDE_GAS_LED,0);
+  digitalWrite(SWITCH_RESIST_HOTWAT_LED,0);
+  digitalWrite(SWITCH_ENERGIA_PRINC_LED,0);
+  digitalWrite(POWER_CONTROL_LED,1);//Encender Fuente de Reles
 }//fin setup---------------------------------
 
 void loop() {
-}
+  switch(estado){
+    case 1:if(Switch_de_Alto_Voltaje(ON,3))estado++;break; //3 segundos
+    case 2:if(Bobina_de_Gas(ON))estado++;break;//encender Bobina de Gas
+    case 3:if(Monitor_Temperatura())estado++;break;//cuenta el tiempo que tarda en llegar la interrupcion del rele de temp.
+    case 4:Ventilador(ON);estado++;break;
+    case 5:if(Monitor_Temperatura2())estado++;break;
+    case 6:break;//estado de Error
+    default:estado=1;break;}//fin de switch loop
+}//fin loop---------------------------------------------------------
+
+//Enciende o apaga el control de alto voltaje de la chispa durante
+//el tiempo que se indique, time1=0 es infinito.
+//state: ON||OFF
+unsigned char Switch_de_Alto_Voltaje(unsigned char state,int time1){
+switch(state){//codigo de seguridad
+  case 1:break;
+  case 0:break;
+  default:state=0;break;}
+  digitalWrite(SWITCH_ENERGIA_PRINC_LED,state);
+if(time1>0){swHV.time1=time1*3;swHV.state=state;}//por 3 es para que sea un seg aprox
+else{swHV.time1=0;}
+return 1;
+}//fin de encender o apagar el apto voltaje-----------------------------
+
+
+//enciende la Bobina de GAS y monitorea la corriente
+//que comprueba que haya bobina
+/* Si la lectura de la corriente de la bobina de Gas no es
+   buena se activa una Alarma de error un LEd y un Buzzer */
+unsigned char Bobina_de_Gas(int estado){
+static unsigned char state,v;//
+unsigned char ret=0;
+  switch(state){
+    case 1:digitalWrite(BOBINA_SOLENOIDE_GAS_LED,ON);state++;break;
+    case 2:if(Lectura_de_Corriente_de_Bobina_de_Gas(&v));state++break;
+    case 3:if(v) ret=1; else ret=0;state++;break;
+    default:state=1;break;}///fin switch
+
+return ret;  
+}//fin de encender la bobina de gas LP-----------------------------------
+
+
 
 void setupTimer() {
   cli();
